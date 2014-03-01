df32beebbf4eccc3a7f8bbe5aef0e31441ef2dd5
Travis Vander Hoop <vanderhoop@me.com>
2014-03-01 13:02:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeocoderTest::Application.config.secret_key_base = '35b27457e747f2e016483570a39ce7c6a087e0ab29859f313c6e6108e9672af2bbf2e01b7a82fb63080dbd18e2264668f876d383abac3b86e9c3646f1f23b8e5'
